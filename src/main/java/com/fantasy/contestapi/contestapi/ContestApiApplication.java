df316261e7872cf2056bc9c23cc6a9938ba7d832
shriyam26 <shriyam1026@gmail.com>
2023-02-24 18:59:16 +0530

    Setup Fantasy project

---
+++ src/main/java/com/fantasy/contestapi/contestapi/ContestApiApplication.java
@@ -0,0 +1,13 @@
+package com.fantasy.contestapi.contestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContestApiApplication.class, args);
+	}
+
+}
