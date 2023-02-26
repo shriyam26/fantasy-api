a1a3d53012e380c6c9ed8f26d0339d7aec3c836d
shriyam26 <shriyam1026@gmail.com>
2023-02-27 00:25:31 +0530

    Mysql setup

--- src/main/java/com/fantasy/contestapi/controller/DemoController.java
+++ src/main/java/com/fantasy/contestapi/controller/DemoController.java
@@ -1,4 +1,4 @@
-package com.fantasy.contestapi.Controller;
+package com.fantasy.contestapi.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoController {
     @RequestMapping(value = "hello")
-    public String hello(){
+    public String hello() {
         return "Spring boot Gradle Example";
     }
 }
